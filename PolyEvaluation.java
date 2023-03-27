fbc39f617d7d1dfc36cf03151c4632c682e1c8c0
TKCoder13 <TKCoding13@gmail.com>
2023-03-26 21:04:50 -0700

    finished the project

--- PolyEvaluation.java
+++ PolyEvaluation.java
@@ -3,7 +3,7 @@ public class PolyEvaluation {
     public static double standardEval(double x) {
         //x -- the input value for the polynomial (given in the prompt)
         int[] a = {3,2,9,6,8,0,4,1};
-        double result = a[0];
+        double result = a[0] + x*a[1];
         double xPower = x;
 
         for (int i = 2; i < a.length; i++) {
@@ -14,67 +14,47 @@ public class PolyEvaluation {
         return result;
     }
 
-    public static double hornersMethod(double x) {
+    public static double hornersMethod(int[] a, double x) {
         //x -- the input value for the polynomial (given in the prompt)
-        int[] a = {3,2,9,6,8,0,4,1};
         int n = a.length - 1;
         double result = a[n];
-        for (int i = n; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             result *= x;
             result += a[i];
         }
         return result;
     }
 
-    //Matrix Multiplication
-    public static double[][] matMult(double[][] A, double[][] B) throws IllegalArgumentException {
-        if (A[0].length != B.length) throw new IllegalArgumentException("Mismatched Arrays");
+    public static double ppCoefs(double x) {
+        int[] t1a = {5,0,0,0,1};
+        double t1 = hornersMethod(t1a, x);
+        int[] t2a = {-1,0,1};
+        double t2 = hornersMethod(t2a, x);
+        int[] t3a = {4,1};
+        double t3 = hornersMethod(t3a, x);
+        int[] t4a = {12,1};
+        double t4 = hornersMethod(t4a, x);
+        int[] t5a = {1,0,1};
+        double t5 = hornersMethod(t5a, x);
+        int[] t6a = {-11,1};
+        double t6 = hornersMethod(t6a, x);
+        int[] t7a = {-26,1};
+        double t7 = hornersMethod(t7a, x);
 
-        double[][] result = new double[A.length][B[0].length];
-        //try catch statement to ensure that the lengths of the array are valid arguments
-        // look at diagram on Marker I
-        try {
-            for (int i = 0; i < A.length; i++) { // pick a row
-                for (int j = 0; j < B[0].length; j++) { // pick a col
-                    for (int k = 0; k < B.length; k++) {
-                        result[i][j] += A[i][k] * B[k][j];
-                    }
-                }
-            }
-            
-            return result;
-
-        } catch (Exception e){
-            throw new IllegalArgumentException("Missmatched Arrays");
-        }
+        return t1*(t2*t3+t4)+(t5*t6+t7);
     }
-
-    public static double preProcCoefs() {
-        double result = 0;
-        return result;
-    }
-
     public static void main(String args[]) {
-        System.out.println("Standard Method: ");
-        
-        for (float i = -20.0f; i <= 20.1; i += 0.2) {
-            System.out.println();
-            System.out.print("i = ");
-            System.out.printf("%.1f", i);
-            System.out.print(", p(");
-            System.out.printf("%.1f", i);
-            System.out.print(") = " + standardEval(i));
-        }
-        System.out.println("\n\nHorner's Method: ");
+
+        System.out.println("Given Polynomial: p(x) = x^7 + 4x^6 + 8x^4 + 6x^3 + 9x^2 + 2x + 3");
 
         for (float i = -20.0f; i <= 20.1; i += 0.2) {
-            System.out.println();
-            System.out.print("i = ");
-            System.out.printf("%.1f", i);
-            System.out.print(", p(");
-            System.out.printf("%.1f", i);
-            System.out.print(") = " + hornersMethod(i));
+            System.out.println(standardEval(i));
+        }
+
+        System.out.println();
+
+        for (float i = -20.0f; i <= 20.1; i += 0.2) {
+            System.out.println(ppCoefs(i));
         }
     }
-
 }
